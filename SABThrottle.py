1aa2adf2f0cd7012d858026ba06531e4c44be2a0
Jason Schollenberger <jason@schollenberger.com>
2014-02-23 13:11:20 -0500

    Add SAB port, decided to scrap active user levels.

--- SABThrottle.py
+++ SABThrottle.py
@@ -9,11 +9,10 @@ PMS_PORT = config['PMS_PORT']
 
 SAB_URL = config['SAB_URL']
 SAB_API = config['SAB_API']
+SAB_PORT = config['SAB_PORT']
 
 PMS_URL = 'http://%s:%s/status/sessions' % (PMS_IP, PMS_PORT)
-SAB_URL = 'http://%s/sabnzbd/api?mode=config&name=speedlimit&apikey=%s&value=' % (SAB_URL, SAB_API)
-
-
+SAB_URL = 'http://%s/sabnzbd:%s/api?mode=config&name=speedlimit&apikey=%s&value=' % (SAB_URL, SAB_API, SAB_PORT)
 
 def get_active_streams(url):
 	dom = minidom.parse(urllib.urlopen(url))
@@ -21,25 +20,13 @@ def get_active_streams(url):
 	for i in total:
 		return int(i.attributes['size'].value)
 		
-active = get_active_streams(PMS_URL)
+active_steams = get_active_streams(PMS_URL)
 
-if active < 1:
-	speed = config['NONE']
-	url = SAB_URL+speed
-	req = urllib2.Request(url)
-	result = urllib2.urlopen(req)
-elif active < 3:
-	speed = config['UNDER_THREE']
-	url = SAB_URL+speed
-	req = urllib2.Request(url)
-	result = urllib2.urlopen(req)
-elif active < 5:
-	speed = config['UNDER_FIVE']
-	url = SAB_URL+speed
-	req = urllib2.Request(url)
-	result = urllib2.urlopen(req)
-elif active < 7:
-	speed = config['UNDER_SEVEN']
-	url = SAB_URL+speed
-	req = urllib2.Request(url)
-	result = urllib2.urlopen(req)
+elif active_steams >= 1:
+	speed = config['LIMIT']
+else:
+	speed = config['NO_LIMIT']
+
+url = SAB_URL+speed
+req = urllib2.Request(url)
+result = urllib2.urlopen(req)
